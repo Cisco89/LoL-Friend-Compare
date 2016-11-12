72f8cd5d71bdbcdb80c5f600e57e3b416ff12531
Cisco <franciscoadriangarcia1@gmail.com>
2016-11-11 23:58:37 -0700

    Mustache

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 
+define( 'BASEPATH', dirname(__FILE__));
+define( 'VIEWPATH', dirname(__FILE__) . '/App/Views');
+
 require 'vendor/autoload.php';
 
+
 use League\Container\Container;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use App\Controllers\UsersController;
+use Zend\Diactoros\Response;
 
 $container = new Container();
 
-$container->share('response', \Zend\Diactoros\Response::class);
+$container->share('UsersController', UsersController::class);
+$container->share('response', Response::class);
 $container->share('request', function (){
    return \Zend\Diactoros\ServerRequestFactory::fromGlobals(
        $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
@@ -19,11 +24,7 @@ $container->share('emitter', \Zend\Diactoros\Response\SapiEmitter::class);
 
 $route = new \League\Route\RouteCollection($container);
 
-$route->map('GET', '/', function(ServerRequestInterface $request, ResponseInterface $response){
-   $response->getBody()->write('<h1>Hello, World!</h1>');
-
-    return $response;
-});
+$route->map('GET', '/', [$container->get('UsersController'), 'create']);
 
 $response = $route->dispatch($container->get('request'), $container->get('response'));
 
