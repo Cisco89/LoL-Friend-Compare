9cbea52cf9390c9ff990bac27ff0072d8407ec46
Cisco <franciscoadriangarcia1@gmail.com>
2017-01-05 22:27:29 -0700

    added additional seed data to database

--- Provision/seeds/seedData.sql
+++ Provision/seeds/seedData.sql
@@ -1,7 +1,10 @@
 INSERT INTO lol_friend_compare.users (username, email, password, last_login, created_at, updated_at) VALUES
 ('Cisco', 'franciscoadriangarcia1@gmail.com', '$2y$10$s3Joj9oP.60v62.6P.q7vO9WltC1zvK/vUIgL6/pq44p/NrsSKJ8y', '2016-12-21 04:30:12', '2016-12-21 03:34:44', '2016-12-21 04:30:12');
 
-insert into division_ranks (`tier`, `division`, `image`) values
+INSERT INTO player_icon(`id`, `image`) VALUES
+(778, 'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/profileicon/778.png');
+
+INSERT INTO division_ranks (`tier`, `division`, `image`) VALUES
 ('Bronze', 'V', ''),
 ('Bronze', 'IV', ''),
 ('Bronze', 'III', ''),
